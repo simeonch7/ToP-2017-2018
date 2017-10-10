5ae0c7d2114066cfdc71bccc174ff02430e1356f
simeonch7 <simeonch7@gmail.com>
2017-10-10 22:40:31 +0300

    From some controllers into 1 and routes gotten working

--- FirstProject/app/controllers/application_controller.rb
+++ FirstProject/app/controllers/application_controller.rb
@@ -1,4 +1,27 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_sessions
-		#so it won't need "authenticity key"
+	protect_from_forgery with: :null_sessions
+	require 'csv'
+
+	def sums
+		
+		sum = 0
+		doc = params[:file]
+		CSV.foreach(doc.path) do |row|
+      			sum += row.first.to_f
+		end
+		sum = sum.ceil
+    	render plain: '%.2f'%sum
+	end
+	
+	def filters
+	doc = params[:file]
+	sum=0;    
+	CSV.foreach(doc.path) do |row|
+		if row[2].to_i % 2!=0 
+		sum+=row[1].to_f
+		end
+	end 
+	sum=sum.ceil
+	render plain: '%.2f'%sum
+	end
 end
--- FirstProject/config/routes.rb
+++ FirstProject/config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :text_files
-  match '/sums', :to => 'sums#create', :via => [:get]
-  match '/filters', :to => 'filters#create', :via => [:get]
-  match '/sums', :to => 'sums#create', :via => [:post]
-  match '/filters', :to => 'filters#create', :via => [:post]
-  match '/intervals', :to => 'intervals#create', :via => [:post]
-  match '/lin_regressions', :to => 'lin_regressions#create', :via => [:post]
+  match '/sums', :to => 'application#sums', :via => [:post]
+  match '/filters', :to => 'application#filters', :via => [:post]
 end
