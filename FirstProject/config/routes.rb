5f2b5275020ef3cce9f927b6dd2cc71577c9d6f3
simeonch7 <simeonch7@gmail.com>
2017-10-10 21:26:14 +0300

    Helpers added and 3rd task

--- FirstProject/config/routes.rb
+++ FirstProject/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	post '/sums'            => 'sums#create'
-	post '/filters'         => 'filters#create'
-	post '/intervals'       => 'intervals#create'
-	post '/lin_regressions' => 'lin_regressions#create'
+  resources :text_files
+  match '/sums', :to => 'sums#create', :via => [:get]
+  match '/filters', :to => 'filters#create', :via => [:get]
+  match '/sums', :to => 'sums#create', :via => [:post]
+  match '/filters', :to => 'filters#create', :via => [:post]
+  match '/intervals', :to => 'intervals#create', :via => [:post]
+  match '/lin_regressions', :to => 'lin_regressions#create', :via => [:post]
 end
