d5386d54fcc3fcf6bdd91060e4e46fe293c1dbb4
simeonch7 <simeonch7@gmail.com>
2017-10-10 18:59:47 +0300

    Added routes

--- FirstProject/config/routes.rb
+++ FirstProject/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	post 'sums'            => 'sums#create'
+	post 'filters'         => 'filters#create'
+	post 'intervals'       => 'intervals#create'
+	post 'lin_regressions' => 'lin_regressions#create'
 end
